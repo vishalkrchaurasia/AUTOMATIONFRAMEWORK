cc92e4bf58d8ee992e2bcba4d860219b40a899c4
javagalbhanu <javagalbhanu@gmail.com>
2018-10-04 09:42:54 +0530

    Data

--- src/script/DemoA.java
+++ src/script/DemoA.java
@@ -1,18 +1,20 @@
 package script;
 
-import org.testng.Assert;
-import org.testng.Reporter;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
 import generic.BaseTest;
-import generic.Result;
+import generic.Utility;
+import page.LoginPage;
 
 
 public class DemoA extends BaseTest{
 	@Test
 	public void testA(){
-		Reporter.log("Executing testA",true);
-		Assert.fail();
+		String un=Utility.getXLData(DATA_PATH,"DemoA",1,0);
+		String pw=Utility.getXLData(DATA_PATH, "DemoA", 1,1);
+		
+		LoginPage l=new LoginPage(driver);
+		l.setUserName(un);
+		l.setPassword(pw);
+		l.clickLogin();
 	}
 }
